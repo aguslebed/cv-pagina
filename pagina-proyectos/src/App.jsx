17b7409ce33ac150c16184571a4e1a07d1fbbf61
AgustinLebed <agus.lebed@gmail.com>
2026-01-12 19:38:32 -0300

    Separacion de las columnas de cada seccion

---
+++ pagina-proyectos/src/App.jsx
@@ -0,0 +1,36 @@
+
+function App() {
+
+  return (
+    <div className="grid grid-cols-10 w-full min-h-screen bg-gray-900">
+      <div id="sidebar" className="col-start-3 col-span-2 bg-red-700">
+        <div id="nombre">
+
+        </div>
+
+        <div id="descripcion">
+
+        </div>
+
+        <div id="navbar">
+
+        </div>
+
+        <div id="contacto">
+
+        </div>
+      </div>
+
+      <div id="main" className="col-start-6 col-span-3 bg-blue-700">
+        <section id="descripcion-extendida">
+
+        </section>
+        <section id="proyectos">
+
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default App
