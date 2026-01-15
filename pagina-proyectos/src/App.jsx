f504c48d0ec9a57544f000dac2cfd29d3ada91b9
AgustinLebed <agus.lebed@gmail.com>
2026-01-15 17:45:56 -0300

    ALgunos iconos y agregue otro proyecto.

--- pagina-proyectos/src/App.jsx
+++ pagina-proyectos/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   const [activeNavItem, setActiveNavItem] = useState("navPresentacion");
   const navItems = [
-    { id: "navPresentacion", label: "Presentacion", href: "#descripcion-extendida" },
+    { id: "navPresentacion", label: "Presentacion", href: "#presentacion" },
     { id: "navProyectos", label: "Proyectos", href: "#proyectos" },
     { id: "navContacto", label: "Contacto", href: "#contacto" },
   ];
@@ -13,8 +13,7 @@ function App() {
     <div className="grid grid-cols-10 w-full min-h-screen bg-linear-to-t 
                     from-gray-900 to-gray-700 text-white font-josefin
                     ">
-      <div id="sidebar" className="col-start-2 col-span-2 mt-20 gap-12 flex 
-      flex-col">
+      <div id="sidebar" className="col-start-2 col-span-2 mt-20 gap-12 flex flex-col sticky top-20 h-[calc(100vh-5rem)]">
         <div className="flex flex-col gap-4">
           <div id="nombre" className="text-6xl">
             <h2>Agustin Lebed</h2>
@@ -46,14 +45,14 @@ function App() {
           </nav>
         </div>
 
-        <div id="contacto" className="flex gap-6 h-full items-end mb-20">
+        <div id="contacto" className="flex gap-6 mt-auto mb-20">
           <a href="https://github.com/aguslebed" target="_blank" rel="noopener noreferrer"><img src="linkedin.png" className="w-8" alt="" /></a>
           <a href="https://github.com/aguslebed" target="_blank" rel="noopener noreferrer"><img src="github.png" className="w-8" alt="" /></a>
         </div>
       </div>
 
       <div id="main" className="col-start-5 col-span-5 mt-20 flex flex-col gap-6">
-        <section id="descripcion-extendida" className="flex flex-col gap-4">
+        <section id="presentacion" className="flex flex-col gap-4">
           <h3 className="text-4xl font-bold">Presentacion</h3>
           <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet officiis, molestiae officia corporis cum voluptatem. Atque, nisi? Illo laboriosam officiis aliquam neque sequi, sit libero quisquam dolores illum rem deserunt laudantium consequuntur architecto aperiam at sed labore, culpa facere aliquid distinctio et earum nulla! Enim repellat non error animi eos?</p>
         </section>
@@ -64,6 +63,12 @@ function App() {
             link={"https://github.com/aguslebed/Chat---Login"}
             tecnologies={["Node.js", "TypeScript", "React", "Vite", "Express", "Socket.io", "MongoDB", "Tailwindcss"]}
             image={"ChatLogin.png"}
+          />
+          <ProjectCard title={"Alfabeto dactologico"}
+            description={"El proyecto desarrolla un sistema de reconocimiento en tiempo real del alfabeto dactilológico de la lengua de señas utilizando la cámara web. A través de Python, OpenCV y MediaPipe, se detecta la posición de la mano y sus puntos de referencia para identificar letras estáticas, mostrando en pantalla la letra reconocida según la postura de la mano."}
+            link={"https://github.com/aguslebed/manos--openCV-"}
+            tecnologies={["Python", "OpenCV", "MediaPipe", "Numpy"]}
+            image={"AlfabetoDactologico.png"}
           />
         </section>
       </div>
--- pagina-proyectos/src/components/ProjectCard.jsx
+++ pagina-proyectos/src/components/ProjectCard.jsx
@@ -1,18 +1,39 @@
 
 
 export default function ProjectCard({ title, description, image, link, tecnologies }) {
+    const icons = {
+        "Node.js": "/tecnologies/Nodejs.svg",
+        "React": "/tecnologies/React.svg",
+        "OpenCV": "/tecnologies/Opencv.png",
+        "Tailwind CSS": "/tecnologies/Tailwindcss.svg",
+        "TypeScript": "/tecnologies/Typescript.svg",
+        "Git": "/tecnologies/Git.svg",
+        "GitHub": "/tecnologies/Github.svg",
+        "Python": "/tecnologies/Python.png",
+        "Postman": "/tecnologies/Postman.svg",
+        "MongoDB": "/tecnologies/Mongodb.png",
+        "Socket.io": "/tecnologies/Socketio.svg",
+        "Tailwindcss": "/tecnologies/Tailwind.svg",
+        "Express": "/tecnologies/Expressjs.png",
+        "Vite": "/tecnologies/Vite.svg",
+        "MediaPipe": "/tecnologies/Mediapipe.png",
+        "Numpy": "/tecnologies/Numpy.png",
+
+    }
+
     return (
-        <div className="flex flex-col gap-4 border  border-gray-500 rounded-lg p-4 shadow-md hover:shadow-lg hover:cursor-pointer hover:scale-105 shadow-gray-400 origin-left transition-all">
+        <div className="flex flex-col gap-4 hover:bg-gray-800 rounded-lg p-4 
+        shadow-md hover:shadow-lg hover:cursor-pointer hover:scale-105 hover:shadow-gray-400 
+        origin-left transition-all" onClick={() => window.open(link, '_blank')}>
             <h3 className="text-2xl">{title}</h3>
             <div className="flex gap-4">
-                <img src={image} alt={title} className="w-72" />
+                <img src={image} alt={title} className="w-72 h-56 rounded-xl" />
                 <p>{description}</p>
 
             </div>
-            <a href={link}>Link al repositorio</a>
             <div className="flex gap-2">
                 {tecnologies.map((tecnology) => (
-                    <span key={tecnology} className="bg-blue-600 px-2 text-white rounded">{tecnology}</span>
+                    <span key={tecnology} className="bg-blue-600 px-2 text-white rounded flex items-center gap-2"><img src={icons[tecnology]} className="w-4 h-4" alt="" />{tecnology}</span>
                 ))}
             </div>
         </div>
